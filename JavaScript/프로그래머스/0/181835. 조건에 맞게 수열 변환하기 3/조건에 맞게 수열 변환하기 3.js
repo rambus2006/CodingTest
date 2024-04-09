cd892be51b8a4d57e52b50bf854ff2ca8eadbe3a
rambus06 <bangminseo50@gmail.com>
2024-04-10 00:08:37 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 5.61 ms, Memory: 45.3 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -0,0 +1,10 @@
+function solution(arr, k) {
+    var answer = [];
+    
+    for(i=0;i<arr.length;i++){
+        if(k%2=="1") answer[i] = arr[i]*k;
+        else answer[i] = arr[i]+k;
+    }
+    
+    return answer;
+}
