de914d51aec07f6b25339b607e5c1dd219a88718
rambus06 <bangminseo50@gmail.com>
2024-08-01 12:00:51 +0900

    [level 2] Title: 전화번호 목록, Time: 425.40 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,14 @@
+function solution(phone_book) {
+    // 전화번호를 정렬합니다.
+    phone_book.sort();
+    
+    // 정렬된 전화번호 배열을 순회하면서 접두어 검사
+    for (let i = 0; i < phone_book.length - 1; i++) {
+        // 현재 번호와 다음 번호를 비교
+        if (phone_book[i + 1].startsWith(phone_book[i])) {
+            return false; // 현재 번호가 다음 번호의 접두어인 경우
+        }
+    }
+    
+    return true; // 접두어인 경우가 없는 경우
+}
