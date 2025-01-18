5f214c35f5807fee9558bd18372434131272677c
rambus06 <bangminseo50@gmail.com>
2025-01-18 22:58:15 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -0,0 +1,13 @@
+def solution(hp):
+    answer = 0
+    if(hp>=5):
+        answer += hp//5
+        hp = hp - 5*(hp//5)
+    if(hp>=3):
+        answer += hp//3
+        hp = hp - 3*(hp//3)
+    if(hp>=1):
+        answer += hp//1
+        hp = hp - 1*(hp//1)   
+        
+    return answer
