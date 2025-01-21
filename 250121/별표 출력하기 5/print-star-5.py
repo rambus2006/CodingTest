af54b9f42b830202136da46f15b4b62610fbd2d6
rambus06 <bangminseo50@gmail.com>
2025-01-21 16:21:35 +0900

    Title: 별표 출력하기 5, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250121/별표 출력하기 5/print-star-5.py
+++ 250121/별표 출력하기 5/print-star-5.py
@@ -1,5 +1,9 @@
 n =int(input())
 a="*"
-for i in range(n,1,-1):
-    print(f"{(a*i)}",f"{a*i}")
-print("*")
+for i in range(n,0,-1):
+    for j in range(i):
+        print(a*i,end=" ")
+    print()
+
+
+
