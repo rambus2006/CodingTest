8a684bf1932d8c94444c3ffb48b9ee0c0433217a
minseo06 <bangminseo50@gmail.com>
2025-05-31 12:22:18 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,30 +1,16 @@
-'''
-100 * 100 의 배열을 만든다.
-위치들을 받아온다. (3,7)...
----> 이부분 반복
-3(x값은 그대로),7은 + 10 = 17 부터 10칸,* 아래로 10칸
-해서 visited로 바꿔준다.
----> 방문한 곳(1인 부분)이 있으면
- +=1을 해주고 출력한다.
-'''
-import sys
+# 색종이 수
+num = int(input())
+paper = [[0] * 100 for _ in range(100)]
 
-grid = [[0]*100 for _ in range(100)]
-loc = []
-aplus = 0
+for i in range(num):
+    W, D = map(int, input().split())
+    for d in range(D,D+10):
+        for w in range(W,W+10):
+                paper[d][w] = 1
+
 cnt = 0
-bplus=0
-n = int(sys.stdin.readline())
-for _ in range(n):
-
-    a,b = list(map(int, input().split()))
-
-    for row in range(a,a+10):
-        for col in range(b,b+10):
-            if grid[row][col] == 0 and 0<=grid[row][col] < 100:
-                grid[row][col] = 1
-
-for i in grid:
-    cnt += i.count(1)
-
+for r in range(100):
+    for c in range(100):
+        if paper[r][c] >= 1:
+            cnt += 1
 print(cnt)
