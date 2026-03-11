f534cf49fd808724863f4c9cfa7d00830444751b
rambus06 <bangminseo50@gmail.com>
2026-03-11 23:45:56 +0900

    [Bronze III] Title: 곱셈, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,7 +1,18 @@
-A = int(input())
-B = input()
+n1 = int(input())
+n2 = int(input())
 
-print(A * int(B[2]))
-print(A * int(B[1]))
-print(A * int(B[0]))
-print(A * int(B))
+r = n2 % 10
+n3 = n1 * r
+
+r = (n2 // 10) % 10
+n4 = n1 * r
+
+r = n2 // 100
+n5 = n1 * r
+
+n6 = n3 + n4 * 10 + n5 * 100
+
+print(n3)
+print(n4)
+print(n5)
+print(n6)
