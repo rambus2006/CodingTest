0218023fc65c797090026264021e358442882e88
rambus06 <bangminseo50@gmail.com>
2026-03-26 16:00:42 +0900

    [Bronze III] Title: 주식 투자, Time: 180 ms, Memory: 111544 KB -BaekjoonHub

--- 백준/Bronze/13416. 주식 투자/주식 투자.py
+++ 백준/Bronze/13416. 주식 투자/주식 투자.py
@@ -1,11 +1,9 @@
 T = int(input())
-
-for _ in range(T):
-    N = int(input())
+for tc in range(1,T+1):
     total = 0
-
-    for _ in range(N):
-        A, B, C = map(int, input().split())
-        total += max(A, B, C, 0)
-
+    N = int(input())
+    for n in range(0,N):
+        li = [0,]
+        li = li + list(map(int,input().split()))
+        total += max(li)
     print(total)
