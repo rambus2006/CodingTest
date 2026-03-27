ee51dc7fec93a230ec560f7f28dd482b0bb55cf5
rambus06 <bangminseo50@gmail.com>
2026-03-27 22:40:31 +0900

    [Silver IV] Title: 동전 0, Time: 80 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,12 +1,11 @@
 N,K = map(int,input().split())
-coins = [ int(input()) for _ in range(0,N)]
+coins = [int(input()) for _ in range(0,N)]
 coins.sort(reverse=True)
 count = 0
 
 for coin in coins:
-    # 만약 K원보다 코인의 값이 작을 때
     if K >= coin:
-        # K를 코인의 값으로 나눴을 때의 몫이 최솟값이 되어야 하는데 이부분을 놓쳤다.
         count += K // coin
-        K = K % coin
+        K %= coin
 print(count)
+# 파이썬 싫어
